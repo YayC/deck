35100c1fb68808d63c575ce65957df1ddc75c0db
J Connolly <j.connolly@mixpanel.com>
2023-08-20 12:29:01 -0700

    update for github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     hmr: {
       port: 443,
-    }
+    },
   },
   build: {
-    outDir: 'dist', // specify the output directory here, 'dist' by default
-  }
-})
+    outDir: "dist", // specify the output directory here, 'dist' by default
+  },
+  base: "/deck/",
+});
